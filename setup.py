ad30685c1bb4e8923fda0ccf1ad2b205fe126159
eazaika <myjabloko@gmail.com>
2018-10-29 00:12:36 +0300

    finally changes

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    install_requires=[
+        'pymongo==3.7.2',
+        'Scrapy==1.5.1',
+        'mongo==0.2.0'
+    ]
 )
