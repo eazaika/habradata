dc4c99caec29b101829b5ecd3d84284324e1b43c
eazaika <myjabloko@gmail.com>
2018-12-24 16:29:02 +0300

    data log message

--- habradata/pipelines.py
+++ habradata/pipelines.py
@@ -23,6 +23,6 @@ class MongoDBPipeline(object):
                 raise DropItem("Missing {0}!".format(data))
         if valid:
             self.collection.insert(dict(item))
-            log.msg("Question added to MongoDB database!",
+            log.msg("OK!",
                     level=log.DEBUG, spider=spider)
         return item
